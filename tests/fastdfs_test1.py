87c81dc9a49b488d6b3a80353e649f00cb69bb38
alex <cyy0523xc@gmail.com>
2020-10-12 14:52:57 +0800

    fastdfs

--- tests/fastdfs_test1.py
+++ tests/fastdfs_test1.py
@@ -6,10 +6,12 @@ Created Time: 2020年09月14日 星期一 15时44分39秒
 '''
 import requests
 
+# 不同的项目，上传地址可能是不一样的
 url = 'http://192.168.80.242:20936/group1/upload'
 files = {
     'file': open('./test.py', 'rb')
 }
+# scene: 该参数有命名空间的效果，根据实际需要传递
 data = {
     'output': 'json',
     'scene': 'pdf'
